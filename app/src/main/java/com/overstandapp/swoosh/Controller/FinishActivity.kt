5b2b9491a19d2432e133c25293d9ec5508ff8439
uwaifo <mr.uwaifo@gmail.com>
2018-07-06 18:28:07 +0100

    draft 5

---
+++ app/src/main/java/com/overstandapp/swoosh/Controller/FinishActivity.kt
@@ -0,0 +1,21 @@
+package com.overstandapp.swoosh.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.overstandapp.swoosh.R
+import com.overstandapp.swoosh.Utilities.EXTRA_LEAGUE
+import com.overstandapp.swoosh.Utilities.EXTRA_SKILL
+import kotlinx.android.synthetic.main.activity_finish.*
+
+class FinishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish)
+
+        val league = intent.getStringExtra(EXTRA_LEAGUE)
+        val skill = intent.getStringExtra(EXTRA_SKILL)
+
+        searchLeagues.text = "Looking for $league  $skill league near you . . . "
+    }
+}
